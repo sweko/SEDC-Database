c4b07dc181b6151d9484bc2130bbb7db08d17c89
sweko <swekster@gmail.com>
2015-05-20 09:09:29 +0200

    Changes to creation script

--- CreateInvoiceDatabase.sql
+++ CreateInvoiceDatabase.sql
@@ -49,7 +49,7 @@ REFERENCES [dbo].[Categories] ([ID])
 GO
 
 CREATE TABLE [dbo].[InvoiceDetails](
-	[ID] [int] NOT NULL,
+	[ID] [int] NOT NULL IDENTITY(1,1),
 	[InvoiceID] [int] NOT NULL,
 	[ProductID] [int] NOT NULL,
 	[Quantity] [int] NOT NULL,
@@ -68,3 +68,8 @@ GO
 ALTER TABLE [dbo].[InvoiceDetails]  WITH CHECK ADD  CONSTRAINT [FK_InvoiceDetails_Products] FOREIGN KEY([ProductID])
 REFERENCES [dbo].[Products] ([ID])
 GO
+
+ALTER TABLE dbo.InvoiceDetails ADD CONSTRAINT UQ_InvoiceDetails UNIQUE NONCLUSTERED (InvoiceID,	ProductID)
+WITH( STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+GO
+
