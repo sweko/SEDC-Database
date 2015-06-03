5d92bb93e5a5170d9723e1e83a195845ef36dab9
sweko <swekster@gmail.com>
2015-06-03 23:45:36 +0200

    Bare Metal

---
+++ AccessingData/BareMetal/Product.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BareMetal
+{
+    public class Product
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public decimal Price { get; set; }
+        public int CategoryID { get; set; }
+
+        public override string ToString()
+        {
+            return string.Format("{0} ({1}) - ${2}", Name, ID, Price);
+        }
+    }
+}
---
+++ AccessingData/BareMetal/Program.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Configuration;
+
+namespace BareMetal
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string cs = ConfigurationManager.ConnectionStrings["TheDrumAndBase"].ConnectionString;
+
+            //var products = new List<Product>();
+            //var categoryID = Console.ReadLine();
+
+           // using (SqlConnection connection = new SqlConnection(cs))
+           // {
+           //     connection.Open();
+
+           //     SqlCommand cmd = new SqlCommand("", connection);
+           //     cmd.CommandType = CommandType.Text;
+           //     cmd.CommandText = "select * from products where categoryId=@p1";
+           //     cmd.Parameters.AddWithValue("@p1", categoryID);
+
+           //     Console.WriteLine(cmd.CommandText);
+           //     var result = cmd.ExecuteReader();
+
+           //     while (result.Read())
+           //     {
+           //         var id = result.GetInt32(0);
+           //         var name = result.GetString(1);
+
+           //         var description = result.IsDBNull(2) 
+           //             ? string.Empty 
+           //             : result.GetString(2);
+
+           //         var price = result.GetDecimal(3);
+           //         var categoryId = result.GetInt32(4);
+
+           //         var product = new Product
+           //         {
+           //             ID = id,
+           //             Name = name,
+           //             Description = description,
+           //             CategoryID = categoryId,
+           //             Price = price
+           //         };
+
+           //         products.Add(product);
+           //     }
+           //}
+
+           // foreach (var product in products)
+           // {
+           //     Console.WriteLine(product);
+           // }
+
+            var id = Console.ReadLine();
+            var price = Console.ReadLine();
+
+            using (SqlConnection connection = new SqlConnection(cs))
+            {
+                connection.Open();
+
+                SqlCommand cmd = new SqlCommand("", connection);
+                cmd.CommandType = CommandType.Text;
+                cmd.CommandText = "update products set price=@price where ID=@id";
+                cmd.Parameters.AddWithValue("@id", id);
+                cmd.Parameters.AddWithValue("@price", price);
+
+                cmd.ExecuteNonQuery();
+            }
+
+        }
+    }
+}
